d348bd7e29bc415086831bbe89c579d698f8a04c
Dylan K. Taylor <odigiman@gmail.com>
2018-05-18 17:14:11 +0100

    Reformat to kill mixed tabs and spaces :skull:

--- src/jojoe77777/SlapBack/Main.php
+++ src/jojoe77777/SlapBack/Main.php
@@ -8,21 +8,21 @@ use pocketmine\plugin\PluginBase;
 use slapper\entities\SlapperHuman;
 use slapper\events\SlapperHitEvent;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener{
 
-    public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
-
-    public function onSlapperHit(SlapperHitEvent $ev){
-    	$entity = $ev->getEntity();
-    	if(!$entity instanceof SlapperHuman){
-    	    return;
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
-    	$pk = new AnimatePacket();
-    	$pk->entityRuntimeId = $entity->getId();
-    	$pk->action = 1;
-    	$ev->getDamager()->dataPacket($pk);
-    }
+
+	public function onSlapperHit(SlapperHitEvent $ev){
+		$entity = $ev->getEntity();
+		if(!$entity instanceof SlapperHuman){
+			return;
+		}
+		$pk = new AnimatePacket();
+		$pk->entityRuntimeId = $entity->getId();
+		$pk->action = 1;
+		$ev->getDamager()->dataPacket($pk);
+	}
 
 }
